95169a543cd5a4cfa50af3fd1594731c696fbcd5
tangshixian <fuxia@sct1039.com>
2024-02-28 10:22:07 +0800

    chore: add vueJsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), vueJsx()],
 	server: {
 		port: 3000,
 	},
 	resolve: {
 		alias: {
-			'@': './src/',
+			'@': path.join(__dirname, 'src'),
 		},
 	},
 })
